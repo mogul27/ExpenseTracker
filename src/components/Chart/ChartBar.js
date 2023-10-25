6a4a583db307e248bb609a326a368f3c2724117c
mogul27 <mogul27442@gmail.com>
2023-10-25 10:05:53 +0100

    Implementing hover box

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
   let barFillHeight = "0%";
+
+  // Set hovering to decide whether to show proportion of spending
+  const [isHovering, setIsHovering] = useState(false);
+
+  const hoverHandler = () => {
+    setIsHovering(true);
+  };
+
+  const noHoverHandler = () => {
+    setIsHovering(false);
+  };
 
   if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.totalValue) * 100) + "%";
@@ -11,15 +22,23 @@ const ChartBar = (props) => {
 
   return (
     <div className="chart-bar">
-      <div className="chart-bar__inner">
+      {isHovering && <p>Housing</p>}
+      <div
+        className="chart-bar__inner"
+        onMouseOver={hoverHandler}
+        onMouseOut={noHoverHandler}
+      >
+        {/* If being hovered over, We create a pop up showing
+      How much of monthly committed spending is that category */}
         <div
           className="chart-bar__fill"
           style={{ height: barFillHeight }}
         ></div>
       </div>
-      <div className="chart-bar__label">{props.label} £{props.value}</div>
-      <div>
+      <div className="chart-bar__label">
+        {props.label} £{props.value}
       </div>
+      <div></div>
     </div>
   );
 };
