0d340693da3d85dc5cdb64c1c292f68aeccf6880
mogul27 <mogul27442@gmail.com>
2023-10-25 14:02:04 +0100

    Removed hover

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -5,28 +5,14 @@ import "./ChartBar.css";
 const ChartBar = (props) => {
   let barFillHeight = "0%";
 
-  // Set hovering to decide whether to show proportion of spending
-  const [isHovering, setIsHovering] = useState(false);
-
-  const hoverHandler = () => {
-    setIsHovering(true);
-  };
-
-  const noHoverHandler = () => {
-    setIsHovering(false);
-  };
-
   if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.totalValue) * 100) + "%";
   }
 
   return (
     <div className="chart-bar">
-      {isHovering && <p>Housing</p>}
       <div
         className="chart-bar__inner"
-        onMouseOver={hoverHandler}
-        onMouseOut={noHoverHandler}
       >
         {/* If being hovered over, We create a pop up showing
       How much of monthly committed spending is that category */}
